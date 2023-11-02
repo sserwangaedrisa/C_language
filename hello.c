c343aede587488160006c04c60d7ed57c7e97419
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-11-02 17:22:55 +0300

    Added the printf function with the place holders

--- hello.c
+++ hello.c
@@ -1,7 +1,13 @@
+#include <cs50.h>
 #include <stdio.h>
+
+
 int main(void)
 {
     printf("hello world \n");
     printf("This is the C language \n");
-    printf("Let's go into it");
+    printf("Let's go into it \n");
+    string firstName = get_string("What is your first name: ");
+    string second = get_string("What is your second name:");
+    printf("hello, %s %s\n", firstName, second);
 }
