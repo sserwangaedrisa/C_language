28fbe7e42c03e99340ede6c82cc4974e18a46763
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-11-05 12:35:47 +0300

    Added the fourth printf statement

--- hello.c
+++ hello.c
@@ -6,6 +6,7 @@ int main(void)
     printf("hello world \n");
     printf("This is the C language \n");
     printf("Let's go into it \n");
+    printf("C is the core language that can make us learn other programing languages easily")
     string firstName = get_string("What is your first name: ");
     string second = get_string("What is your second name:");
     printf("hello, %s %s\n", firstName, second);
