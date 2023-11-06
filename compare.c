0060f298e163c80a34566bcb40cacda35b93fdd2
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-11-06 14:21:25 +0300

    Added more conditions

--- compare.c
+++ compare.c
@@ -5,8 +5,14 @@ int main(void)
 {
     int x = get_int("What's x? ");
     int y = get_int("What's y? ");
-    if (x<y)
+    if ( x < y)
     {
         printf("x is less than y \n");
+    }
+    else if (x > y) {
+        printf("x is greater than y \n");
+    }
+    else {
+        printf("x is equal to y");
     }
 }
