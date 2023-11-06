77de2b531b9e9f2c24cb13d31e3a9e0225358b42
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-11-06 14:14:54 +0300

    added a comparing file

---
+++ compare.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int x = get_int("What's x? ");
+    int y = get_int("What's y? ");
+    if (x<y)
+    {
+        printf("x is less than y \n");
+    }
+}
--- hello.c
+++ hello.c
@@ -5,9 +5,10 @@ int main(void)
 {
     printf("hello world \n");
     printf("This is the C language \n");
+    printf("HE");
     printf("Let's go into it \n");
-    printf("C is the core language that can make us learn other programing languages easily")
-    string firstName = get_string("What is your first name: ");
+    printf("C is the core language that can make us learn other programing languages easily");
+
     string second = get_string("What is your second name:");
-    printf("hello, %s %s\n", firstName, second);
+    printf("hello, %s\n",second);
 }
