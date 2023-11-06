90780d8ea1d6ccbc7b4bf375385c6028dc019744
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-11-06 16:03:29 +0300

    Added the file containing the loop manuplations

---
+++ loops.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+    //while loops 
+    int i = 0 ;
+    while (i < 5){
+        printf("I'm in the  loop \n");
+        i++;
+    }
+    printf(" That was a while loop \n");
+
+
+    for (int y = 0; y < 5; y++) {
+        printf("This is the for loop\n");
+    }
+    printf("That was a for loop");
+}
