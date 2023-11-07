0ed770221f61a73393df1e41899ba2a6cdca1015
SSERWANGA EDIRISA <sserwangaedrisa@gmail.com>
2023-11-07 12:19:09 +0300

    Update loops.c

--- loops.c
+++ loops.c
@@ -3,14 +3,15 @@ int main(void)
 {
     //while loops 
     int i = 0 ;
-    while (i < 5){
+    int n =5;
+    while (i < n){
         printf("I'm in the  loop \n");
         i++;
     }
     printf(" That was a while loop \n");
 
 
-    for (int y = 0; y < 5; y++) {
+    for (int y = 0; y < n; y++) {
         printf("This is the for loop\n");
     }
     printf("That was a for loop");
