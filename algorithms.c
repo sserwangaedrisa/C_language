4e6cb7e5ece95e3d0d5a34b4ac3c326762c095af
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-11-12 13:48:01 +0300

    Made a function to check for the presence of the number in an array

--- algorithms.c
+++ algorithms.c
@@ -3,5 +3,20 @@
 // In this file we are going to show how algorithms are built from the basic premitives 
 
 int main(void){
-    
+    int array[] = { 30, 49, 84, 200 };
+    int number = get_int(" Enter the number: ");
+    for (int i = 0; i < 4; i++) {
+        if (array[i] == number) {
+            printf("The number is in the array\n");
+            return 0;
+        }
+
+        
+        printf("The number is not found \n");
+        return 2;
+
+
+    }
+
+    return 2;
 }
