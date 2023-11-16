45ac97a521404f73c441504a82ef0aa29f2e872c
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-11-16 23:27:23 +0300

    Updated the loops.c file

--- loops.c
+++ loops.c
@@ -9,7 +9,8 @@ int main(void)
         i++;
     }
     printf(" That was a while loop \n");
-
+    // For loops , these loops are mainly used when the condition can go up the defined number of times that are with in the certain boundary
+    // for example; for ( int i=o; i< 4; i++) This means that when the  integer i is less than 4, let the loop remain running
 
     for (int y = 0; y < n; y++) {
         printf("This is the for loop\n");
