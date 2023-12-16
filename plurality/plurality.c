3f6ec98ed957ec5a8f3ec83f504bdfe8558ae6c2
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-12-16 16:12:32 +0300

    updated the file

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -19,3 +19,19 @@ int candidate_count;
 // Function prototypes
 bool vote(string name);
 void print_winner(void);
+int main(int argc, string argv[])
+{
+    // Check for invalid usage
+    if (argc < 2)
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
+    // Populate array of candidates
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
