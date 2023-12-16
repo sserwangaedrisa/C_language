a1da62b6307abdb1885454d56d60bb0c83c609d3
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-12-16 16:10:48 +0300

    Added the max no. and candidate data structure

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -1,3 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+// Max number of candidates
+#define MAX 9
+
+// Candidates have name and vote count
+typedef struct
+{
+    string name;
+    int votes;
+} candidate;
