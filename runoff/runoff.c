7ff06f28b358876684c1016ff025767f3cb8d571
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-12-17 16:39:43 +0300

    updated the file

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -181,7 +181,7 @@ bool print_winner(void)
     return false;
 }
 
-// Return the minimum number of votes any remaining candidate has.
+// Returning the minimum number of votes any remaining candidate has.
 int find_min(void)
 {
     // TODO
