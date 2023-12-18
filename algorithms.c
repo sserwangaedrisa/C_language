4899b1216f1ecb7945f0efbbb92db66278561f8e
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-12-18 20:12:01 +0300

    Updated the code

--- algorithms.c
+++ algorithms.c
@@ -1,6 +1,5 @@
 #include <cs50.h>
 #include <stdio.h>
-// In this file we are going to show how algorithms are built from the basic premitives 
 
 int main(void){
     int array[] = { 30, 49, 84, 200 };
