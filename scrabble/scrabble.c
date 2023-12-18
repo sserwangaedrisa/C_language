bbce79ae882f3e96637217b8406e7fc5a7178bae
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-12-18 18:48:06 +0300

    Updated the code

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,7 +5,7 @@
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-
+// Initialising the compute function.
 int compute_score(string word);
 
 int main(void)
