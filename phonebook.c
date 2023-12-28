7eb916775e4aaf3cfa6766e8b31d178429b6dac6
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-12-28 22:10:34 +0300

    Added the file phonebook.c with its code that allows the writing of phone no.s to the phonebook.csv

---
+++ phonebook.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    FILE*file =fopen("phonebook.csv", "a");
+    sting name = get_string("Name: ");
+    string contact = get_string("Contact: ");
+
+    fprintf(file, "%s , %s\n", name , contact);
+    fclose(file);
+}
