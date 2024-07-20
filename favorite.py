6e17084b5c61b6ce07f024ef5352d5cf5d9f71d8
sserwangaedrisa <sserwangaedrisa@gmail.com>
2024-07-20 22:32:41 +0400

    new commit

--- favorite.py
+++ favorite.py
@@ -5,11 +5,40 @@ with open("favorites.csv", "r") as file:
     # next(currentRow)
     # for row in currentRow:
     #     print(row[1])
+
     # bellow is the modification of the above code wc introduces the dict datatype in the code
+    # currentRow = csv.DictReader(file)
+    # for row in currentRow:
+    #     print(row["language"])
+
+    # COUNTING THE LANGUAGES AND THEIR VOTES
+#     currentRow = csv.DictReader(file)
+#     scratch, python, c = 0, 0, 0
+#     for row in currentRow:
+#         lang = row["language"]
+#         if lang == 'Scratch':
+#             scratch += 1
+#         elif lang == 'Python':
+#             python += 1
+#         elif lang == 'C':
+#             c += 1
+
+
+# print(f"Scratch: {scratch}")
+# print(f"Python: {python}")
+# print(f"C: {c}")
+
+    # SIMPLIFYING THE CODE WITH THE USE OF ONE DICT TO TRACK MORE THINGS 
+
     currentRow = csv.DictReader(file)
+    count = {}
+    
     for row in currentRow:
-        print(row["language"])
+        favorite = row['language']
+        if favorite in count:
+            count[favorite] += 1
+        else:
+            count[favorite] = 1
 
-
-
- 
+    for favorite in count:
+        print(f"{favorite}: {count[favorite]}")
