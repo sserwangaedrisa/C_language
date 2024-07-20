29aed37eb29ffd96198efc0b1340632faec7eaa4
sserwangaedrisa <sserwangaedrisa@gmail.com>
2024-07-20 20:39:31 +0400

    ww

--- favorite.py
+++ favorite.py
@@ -1,9 +1,15 @@
 import csv
 
 with open("favorites.csv", "r") as file:
-    currentRow = csv.reader(file)
-    next(currentRow)
+    # currentRow = csv.reader(file)
+    # next(currentRow)
+    # for row in currentRow:
+    #     print(row[1])
+    # bellow is the modification of the above code wc introduces the dict datatype in the code
+    currentRow = csv.DictReader(file)
     for row in currentRow:
-        print(row[1])
+        print(row["language"])
+
+
 
  
