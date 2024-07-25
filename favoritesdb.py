0b4d46223df65fe8e686a2172d61da8c0634d057
sserwangaedrisa <sserwangaedrisa@gmail.com>
2024-07-25 19:46:18 +0400

    hh

--- favoritesdb.py
+++ favoritesdb.py
@@ -1,7 +1,9 @@
 from cs50 import SQL
 
 favorite = input("Favorite:")
+# openning the database file in python 
 db = SQL("sqlite:///favorites.db")
+
 row = db.execute("select count(*) as count from favorites where problem =?", favorite)
 row = row[0]
 
