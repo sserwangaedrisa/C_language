f2a6805586ddd9dff684301dfea27a1399bebf09
sserwangaedrisa <sserwangaedrisa@gmail.com>
2024-07-25 19:48:12 +0400

    Added some comments in the file

--- favoritesdb.py
+++ favoritesdb.py
@@ -3,7 +3,7 @@ from cs50 import SQL
 favorite = input("Favorite:")
 # openning the database file in python 
 db = SQL("sqlite:///favorites.db")
-
+# executing sql queries in python.
 row = db.execute("select count(*) as count from favorites where problem =?", favorite)
 row = row[0]
 
